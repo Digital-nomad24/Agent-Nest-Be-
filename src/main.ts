759db94e7ad035487358bd72ba9a6141ba661ffb
Nakshatra Vashisth <nakshvashisth@gmail.com>
2025-12-28 17:40:01 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     whitelist:true
   }))
   app.enableCors({
-    origin: ['http://localhost:8080','http://localhost:8000'], // frontend URL
+    origin: ['*','http://localhost:8080','http://localhost:8000'], // frontend URL
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
