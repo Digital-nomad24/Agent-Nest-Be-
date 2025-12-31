66f62c8bff187b161c2e03e01738dae8fae24a2e
Nakshatra Vashisth <nakshvashisth@gmail.com>
2025-12-31 19:56:27 +0530

    rollback

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -4,13 +4,12 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Profile, VerifyCallback } from 'passport-google-oauth20';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(
     private configService: ConfigService,
     private prisma: PrismaService,
-    private JwtService:JwtService
   ) {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID')!,
@@ -22,28 +21,12 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     });
   }
 
-  authorizationParams(req: any): { [key: string]: string } {
-  const { flow, redirectTo } = req.query;
-
-  if (flow && redirectTo) {
-    const state =this.JwtService.sign(
-      { flow, redirectTo },
-      { expiresIn: '1h' },
-    );
-
+  authorizationParams(): { [key: string]: string } {
     return {
       access_type: 'offline',
       prompt: 'consent',
-      state,
     };
   }
-
-  return {
-    access_type: 'offline',
-    prompt: 'consent',
-  };
-}
-
 
   async validate(
     req: any, // ✅ Now we get the request object
