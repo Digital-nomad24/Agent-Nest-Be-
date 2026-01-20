23b2d10e9390e4ee627a5fca4b8ca843c6f36e9f
Nakshatra Vashisth <nakshvashisth@gmail.com>
2026-01-20 23:08:12 +0530

    another push

--- src/meeting/meeting.controller.ts
+++ src/meeting/meeting.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post } from '@nestjs/common';
+import { GoogleCalendarClientService } from 'src/calendar/services/getGoogleCalendarClient.service';
 @Controller('meeting')
-export class MeetingController {}
+export class MeetingController 
+{
+    constructor(private readonly GoogleCalendarClientService: GoogleCalendarClientService) {}
+
+    @Post('bookMeeting')
+    async bookMeeting(){
+        
+    }
+}
+
