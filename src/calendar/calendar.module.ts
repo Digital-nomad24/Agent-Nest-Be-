0d5f0ef052b1b95e3e4b16bfa7ae4af216ee70e0
Nakshatra Vashisth <nakshvashisth@gmail.com>
2026-01-25 17:35:22 +0530

    added the meeting controller and service

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -1,27 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CalendarController } from './controllers/calendar.controller';
 import { USE_CASE_PROVIDER } from './use-cases/use-cases.provider';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { SERVICE_PROVIDER } from './services/services.provider';
-import { GoogleCalendarService } from 'src/google/services/google-calendar.service';
 import { GoogleModule } from 'src/google/google.module';
 import { GoogleCalendarClientService } from './services/getGoogleCalendarClient.service';
 
 @Module({
-  imports:[GoogleModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }), 
-        JwtModule.registerAsync({
-          imports: [ConfigModule],
-          useFactory: async (configService: ConfigService) => ({
-            secret: configService.get<string>('JWT_SECRET'),
-            signOptions: { expiresIn: '7d' },
-          }),
-          inject: [ConfigService],
-        })
-    
-  ],
+  imports:[GoogleModule],
   controllers: [CalendarController],
   exports:[GoogleCalendarClientService],
   providers: [...SERVICE_PROVIDER,...USE_CASE_PROVIDER,],
--- src/meeting/meeting.module.ts
+++ src/meeting/meeting.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MeetingController } from './meeting.controller';
 import { CalendarModule } from 'src/calendar/calendar.module';
+import { MeetingService } from './meeting.service';
 
 @Module({
   imports: [CalendarModule],
   controllers: [MeetingController],
-  providers: []
+  providers: [MeetingService]
 })
 export class MeetingModule {}
