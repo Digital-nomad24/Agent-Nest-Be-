911e2f0cafb985bf0294e84543cb1391eb29b09c
Nakshatra Vashisth <nakshvashisth@gmail.com>
2026-01-25 17:35:33 +0530

    meeting module

--- src/meeting/meeting.module.ts
+++ src/meeting/meeting.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MeetingController } from './meeting.controller';
+import { MeetingController } from './controller/meeting.controller';
 import { CalendarModule } from 'src/calendar/calendar.module';
 import { MeetingService } from './meeting.service';
 
